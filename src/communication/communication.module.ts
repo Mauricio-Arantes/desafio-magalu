bef7bfd7f82558857c0c9cc1eb9fe964afe5037c
mauricioarantes <noob-crispim@hotmail.com>
2022-01-25 23:05:28 -0300

    Iniciando middleware de autenticação

--- src/communication/communication.module.ts
+++ src/communication/communication.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CommunicationService } from './communication.service';
 import { CommunicationController } from './communication.controller';
+import { LoggerMiddleware } from '../middleware/logger.middleware';
 
 @Module({
   controllers: [CommunicationController],
   providers: [CommunicationService],
 })
-export class CommunicationModule {}
+export class CommunicationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('communication');
+  }
+}
