8f5b0fc6a6e65f53341036ecd2499aaa76ecfcad
mauricioarantes <noob-crispim@hotmail.com>
2022-01-26 23:58:23 -0300

    Provendo o client do prisma para os services

--- src/communication/communication.module.ts
+++ src/communication/communication.module.ts
@@ -2,10 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CommunicationService } from './communication.service';
 import { CommunicationController } from './communication.controller';
 import { LoggerMiddleware } from '../middleware/logger.middleware';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [CommunicationController],
-  providers: [CommunicationService],
+  providers: [CommunicationService, PrismaService],
 })
 export class CommunicationModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
