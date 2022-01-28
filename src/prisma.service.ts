eceabe5b309e6cafa6b79816b1a8b0a9d75e9a35
mauricioarantes <noob-crispim@hotmail.com>
2022-01-28 03:07:29 -0300

    midleware de delete

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -10,6 +10,24 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    this.$use(async (params, next) => {
+      if (params.action == 'delete') {
+        params.action = 'update';
+        params.args['data'] = {
+          ...params.args['data'],
+          deleted_at: Date.now(),
+        };
+      }
+      if (params.action == 'deleteMany') {
+        params.action = 'updateMany';
+        if (params.args.data != undefined) {
+          params.args.data['deleted_at'] = Date.now();
+        } else {
+          params.args['data'] = { deleted_at: Date.now() };
+        }
+      }
+      return next(params);
+    });
   }
 
   async enableShutdonHooks(app: INestApplication) {
