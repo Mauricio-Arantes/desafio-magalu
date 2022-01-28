bcf36fbb01a040333ff3378318164752c0d4ef69
mauricioarantes <noob-crispim@hotmail.com>
2022-01-28 11:13:11 -0300

    middleware de soft delete

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -11,11 +11,12 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
     this.$use(async (params, next) => {
+      // Soft delete middleware
       if (params.action == 'delete') {
         params.action = 'update';
         params.args['data'] = {
           ...params.args['data'],
-          deleted_at: Date.now(),
+          deleted_at: new Date(),
         };
       }
       if (params.action == 'deleteMany') {
@@ -25,6 +26,31 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
         } else {
           params.args['data'] = { deleted_at: Date.now() };
         }
+      }
+
+      // Not searching soft delete datas
+      let findMethod = false;
+      switch (params.action) {
+        case 'findUnique':
+          params.action = 'findFirst';
+          findMethod = true;
+          break;
+        case 'update':
+          params.action = 'updateMany';
+          findMethod = true;
+          break;
+        case 'findMany':
+          findMethod = true;
+          break;
+        case 'updateMany':
+          findMethod = true;
+          break;
+      }
+      if (findMethod) {
+        params.args['where'] = {
+          ...params.args['where'],
+          deleted_at: null,
+        };
       }
       return next(params);
     });
