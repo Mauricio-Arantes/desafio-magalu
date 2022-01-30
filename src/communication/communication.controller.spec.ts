5cb50430abcc64d9bfc6a335d5e10fc1c94aca1f
mauricioarantes <noob-crispim@hotmail.com>
2022-01-30 15:17:28 -0300

    test: :construction: Testes unitários do controller

--- src/communication/communication.controller.spec.ts
+++ src/communication/communication.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Prisma, CommunicationTypes, Communications } from '@prisma/client';
+import { CommunicationTypes } from '@prisma/client';
 
 import { PrismaService } from '../database/prisma.service';
 import { CommunicationController } from './communication.controller';
@@ -8,6 +8,7 @@ import { CreateCommunicationDto } from './dto/create-communication.dto';
 
 describe('CommunicationController', () => {
   let controller: CommunicationController;
+  let service: CommunicationService;
 
   const expectedMinimalDto: CreateCommunicationDto = {
     shipping_date: new Date().toString(),
@@ -18,19 +19,39 @@ describe('CommunicationController', () => {
   };
 
   const expectedCompleteDto: CreateCommunicationDto = {
-    shipping_date: new Date().toString(),
-    recipient: 'Boneco de teste do posto',
+    ...expectedMinimalDto,
     message: {
-      type: CommunicationTypes.EMAIL,
+      type: CommunicationTypes.PUSH,
       content: 'Ta maluco ta doidão',
     },
   };
 
+  const wrongDto = {
+    ...expectedMinimalDto,
+    message: null,
+  };
+
+  const commomResponse = {
+    id: '19bbd769-55c8-4781-8921-d5337a15c269',
+    shipping_date: '2022-01-28T02:23:56.747Z',
+    recipient: 'Boneco de teste',
+    status: 'PENDING',
+    deleted_at: null,
+    updated_at: '2022-01-28T02:23:56.794Z',
+    created_at: '2022-01-28T02:23:56.793Z',
+    message_id: '1dd15369-94e5-431f-b9ab-18d9c259405e',
+    message: {
+      id: '1dd15369-94e5-431f-b9ab-18d9c259405e',
+      type: 'EMAIL',
+      content: null,
+      deleted_at: null,
+      updated_at: '2022-01-28T02:23:56.794Z',
+      created_at: '2022-01-28T02:23:56.793Z',
+    },
+  };
   const mockCommunicationService = {
-    create: jest.fn((dto) => {
-      return dto;
-    }),
-    findAll: jest.fn((maxValue, initialValue) => {}),
+    create: jest.fn().mockResolvedValue(commomResponse),
+    findAll: jest.fn().mockResolvedValue([commomResponse]),
   };
 
   beforeEach(async () => {
@@ -43,16 +64,56 @@ describe('CommunicationController', () => {
       .compile();
 
     controller = module.get<CommunicationController>(CommunicationController);
+    service = module.get<CommunicationService>(CommunicationService);
   });
 
   it('should be instancied', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 
-  it('should receive the DTO expected data', () => {
-    expect(controller.create(expectedMinimalDto)).toBeInstanceOf(
-      Prisma.CommunicationsGetPayload,
-    );
+  describe('Create', () => {
+    it('should create a new communication', async () => {
+      const result = await controller.create(expectedMinimalDto);
+
+      expect(result).toEqual(commomResponse);
+      expect(service.create).toHaveBeenCalledTimes(1);
+      expect(service.create).toBeCalledWith(expectedMinimalDto);
+    });
+
+    it('should throw an exception', () => {
+      jest.spyOn(service, 'create').mockRejectedValueOnce(new Error());
+
+      expect(controller.create(expectedMinimalDto)).rejects.toThrowError();
+    });
+  });
+
+  describe('FindAll', () => {
+    it('FindAll should return a array of communication', async () => {
+      const result = await controller.findAll(0, 0);
+
+      expect(result).toEqual([commomResponse]);
+    });
+
+    it('should throw a exception', () => {
+      jest.spyOn(service, 'findAll').mockRejectedValueOnce(new Error());
+
+      expect(controller.findAll(0, 0)).rejects.toThrowError();
+    });
+
+    // it('should throw a error when passed the wrong parameters', () => {
+    //   expect(controller.findAll('a', 'c')).rejects.toThrowError();
+    // });
+  });
+
+  it('POST should redirect the DTO to a called function', () => {
+    expect(controller.create(expectedMinimalDto)).toBeDefined();
+    expect(controller.create(expectedCompleteDto)).toBeDefined();
+  });
+
+  it('POST should redirect the DTO to a called function', () => {
+    expect(controller.create(expectedMinimalDto)).toBeDefined();
+    expect(controller.create(expectedCompleteDto)).toBeDefined();
   });
 
   it('should get all users', () => {
