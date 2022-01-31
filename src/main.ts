ce6b6718686c532ef023c7ddcd56131bca2b730c
mauricioarantes <noob-crispim@hotmail.com>
2022-01-31 06:47:08 -0300

    build: :ambulance: arquivo inicializador do projeto precisa ficar no root

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { MainModule } from '../main.module';
+import { MainModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
