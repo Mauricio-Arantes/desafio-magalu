55b0bdf65b2f66f9dc9f0aa6c5679bef7b4e61c8
mauricioarantes <noob-crispim@hotmail.com>
2022-01-31 13:23:47 -0300

    refactor: atualizando update

--- src/communication/dto/update-communication.dto.ts
+++ src/communication/dto/update-communication.dto.ts
@@ -1,16 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CommunicationStatus, CommunicationTypes } from '@prisma/client';
 
 import { CreateCommunicationDto } from './create-communication.dto';
 
 export class UpdateCommunicationDto extends PartialType(
   CreateCommunicationDto,
-) {
-  shipping_date: string;
-  status?: CommunicationStatus;
-  recipient: string;
-  message: {
-    type: CommunicationTypes;
-    content?: string;
-  };
-}
+) {}
